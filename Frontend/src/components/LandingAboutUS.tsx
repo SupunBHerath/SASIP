1c4845d8a9923bd1d4946c09bb77de993506157d
Heshan-404 <heshantharushka2002@gmail.com>
2024-04-25 00:59:50 +0530

    about us

---
+++ Frontend/src/components/LandingAboutUS.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+export default function Sasip() {
+  useState();
+  return (
+    <>
+      <button className="btn btn-danger">asdsa</button>
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe
+          className="embed-responsive-item"
+          src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
+        ></iframe>
+      </div>
+    </>
+  );
+}
