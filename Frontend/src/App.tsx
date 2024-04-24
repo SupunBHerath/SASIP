b784f856005cb62f90640d1bf76c0b5ef2671013
Supun B herath <136695038+SupunBHerath@users.noreply.github.com>
2024-04-25 04:37:51 +0530

    add navibar

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,16 +1,20 @@
-import Sasip from './Sasip'
+// import Sasip from './Sasip'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginForm from './pages/login'
+
+import Navibar from './components/Navibar/Navibar'
 
 const App = () => {
   return (
     <div>
+      <Navibar/>
       <BrowserRouter>
         <Routes>
+          {/* <Route path='/navi' element={}/> */}
           <Route path='/login' element={<LoginForm/>}/>
         </Routes>
       </BrowserRouter>
-      <Sasip/>
+      {/* <Sasip/> */}
     </div>
   )
 }
