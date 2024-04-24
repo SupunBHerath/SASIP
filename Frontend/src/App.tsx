1914d8c61cf2810b997d444a8ef1880e7649f6d0
dprasad988 <154485871+dprasad988@users.noreply.github.com>
2024-04-25 01:37:44 +0530

    .

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
 import Sasip from './Sasip'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginForm from './pages/login'
 
 const App = () => {
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<LoginForm/>}/>
+        </Routes>
+      </BrowserRouter>
       <Sasip/>
     </div>
   )
