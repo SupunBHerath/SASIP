428dd4ce0350c53a40c47188a55767db5e34ac73
Supun B herath <136695038+SupunBHerath@users.noreply.github.com>
2024-04-25 12:28:53 +0530

    change hero

--- Frontend/src/components/Sideshow/Sideshow.tsx
+++ Frontend/src/components/Sideshow/Sideshow.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Logo from '../../assets/sasip-logo.jpeg'
+
 import LeftImg from './LeftImg'
+import './Sideshow.css'
+import Rightimg from './RightImg';
 const Sideshow = () => {
+
   return (
     <div>
+      
       <div id="carouselExampleDark" className="carousel carousel-dark slide">
   <div className="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
@@ -11,22 +16,25 @@ const Sideshow = () => {
     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
   <div className="carousel-inner">
-    <div className="carousel-item active" data-bs-interval="10000">
+    <div className="carousel-item active" data-bs-interval="10000"  >
       {/* <img src={Logo} className="d-block w-100  " alt="..." style={{height:"640px"}}/> */}
-      <LeftImg img={Logo} title="SASIP" desc="abnamnvamn"/>
+     
+    <Rightimg img={Logo} title="Experience the Future of  Quality Online Learning!" desc="abnamnvamn"/>
     </div>
-    <div className="carousel-item" data-bs-interval="2000">
-    <LeftImg img={Logo} title="SASIP" desc="abnamnvamn"/>
+    <div className="carousel-item" data-bs-interval="2000" >
+    <LeftImg img={Logo} title="Experience the Future of  Quality Online Learning!" desc="abnamnvamn"/>
+
     </div>
-    <div className="carousel-item">
-    <LeftImg img={Logo} title="SASIP" desc="abnamnvamn"/>
+    <div className="carousel-item" >
+    <Rightimg img={Logo} title="Experience the Future of  Quality Online Learning!" desc="abnamnvamn"/>
+
     </div>
   </div>
   {/* <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
     <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+  </button> */}
+  {/* <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
     <span className="carousel-control-next-icon" aria-hidden="true"></span>
     <span className="visually-hidden">Next</span>
   </button> */}
