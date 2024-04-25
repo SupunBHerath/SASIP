800db49b708400f4c5429b246c848fa727799809
dprasad988 <154485871+dprasad988@users.noreply.github.com>
2024-04-25 12:10:55 +0530

    .

--- Frontend/src/pages/adminDashboard.tsx
+++ Frontend/src/pages/adminDashboard.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
-function AdminDashboard() {
+const AdminDashboard: React.FC = () => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
     </div>
-  )
-}
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard; // Export the component
