9db3bcdb2befcf8e8a8c3464ebd12a3c4a538b52
Heshan-404 <heshantharushka2002@gmail.com>
2024-07-15 14:41:46 +0530

    .

--- Frontend/src/Comporant/Landing/about/AboutCard.jsx
+++ Frontend/src/Comporant/Landing/about/AboutCard.jsx
@@ -1,28 +1,34 @@
-import React from "react"
-import Heading from "../common/heading/Heading"
-import "./about.css"
-import { homeAbout } from "../dummydata"
-import Awrapper from "./Awrapper"
+import React from "react";
+import Heading from "../common/heading/Heading";
+import "./about.css";
+import { homeAbout } from "../dummydata";
+import Awrapper from "./Awrapper";
 
 const AboutCard = () => {
   return (
     <>
-      <section className='aboutHome'>
-        <div className='container flexSB'>
-          <div className='left row'>
-            <img src='../public/image/logoe1.png' alt='Logo'/>
+      <section className="aboutHome">
+        <div className="container flexSB">
+          <div className="left row">
+            <img src="../public/image/logoe1.png" alt="Logo" />
           </div>
-          <div className='right row'>
-            <center><h1 className="titlem">WHO WE ARE</h1></center>
-            <div className='items'>
+          <div className="right row">
+            <center>
+              <h1 className="titlem" style={{ fontSize: "30px" }}>
+                WHO WE ARE
+              </h1>
+            </center>
+            <div className="items">
               {homeAbout.map((val, index) => {
                 return (
-                  <div className='item flexSB' key={val.id || index}>
-                    <div className='text'>
-                      <center><p>{val.desc}</p></center>
+                  <div className="item flexSB" key={val.id || index}>
+                    <div className="text">
+                      <center>
+                        <p>{val.desc}</p>
+                      </center>
                     </div>
                   </div>
-                )
+                );
               })}
             </div>
           </div>
@@ -30,7 +36,7 @@ const AboutCard = () => {
       </section>
       <Awrapper />
     </>
-  )
-}
+  );
+};
 
-export default AboutCard
+export default AboutCard;
