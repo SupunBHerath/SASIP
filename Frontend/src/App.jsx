ee790eb5d39e531a3e76f9fde113682064ea3922
BihanMadhusankha <ellawalagemadhusankha02@gmail.com>
2024-07-17 02:57:15 +0530

    update about and contact us and app js

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import AdminHome from './Pages/Admin/AdminHome'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import SignIn from './Pages/Admin/AdminLogin';
 import TimeTablePage from './Pages/User/TimeTablePage';
 import Home from './Pages/User/UserHome';
 import Team from './Pages/team/Team';
-import ContactPage from './Pages/User/AboutUsPage';
+import ContactPage from './Comporant/Landing/contact/Contact';
 import Profile from './Pages/User/LecturerProfile';
+import ResetPassword from "./Pages/Admin/ResetPassword";
 
 import {AuthProvider} from './Config/AuthContext';
 import About from './Comporant/About/AboutUs';
@@ -29,9 +29,18 @@ export default function App() {
 
         
           {/* =====================Admin====================== */}
-          <Route path="/admin" element={<PrivateRoute element={<AdminHome />} />} />
+          {/* <Route
+            path="/admin"
+            element={<PrivateRoute element={<AdminHome />} />}
+          />
+          />{" "} */}
+          {/* Use PrivateRoute for AdminHome */}
+          <Route path="/admin" element={<AdminHome />} />
           <Route path="/admin/login" element={<SignIn />} />
           <Route path="/reset-password/:token" element={<ResetPassword />} />
+          <Route path="/profile" element={<Profile />} />
+          {/* <Route path="/" element={<Profile />} /> */}
+
         </Routes>
       </BrowserRouter>
     </AuthProvider>
