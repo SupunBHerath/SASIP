c7ddd95abf9a151e44e4988574b9da2c71172f59
dprasad988 <154485871+dprasad988@users.noreply.github.com>
2024-07-18 20:42:52 +0530

    ..

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from 'dotenv';
 import authRoutes from "./Routes/authRoutes.js"
+import albumRoutes from "./Routes/albumRoutes.js"
 
 dotenv.config();
 
@@ -12,6 +13,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api', authRoutes); 
+app.use('/api', albumRoutes); 
+app.use('/api/albums', albumRoutes); 
+
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
