89d3540ae402fd00c817dcb7d40ee163a949348f
dprasad988 <154485871+dprasad988@users.noreply.github.com>
2024-07-20 18:11:30 +0530

    update

--- Backend/Routes/timetableRoutes.js
+++ Backend/Routes/timetableRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { addTimetableEntries, getAllTimetableEntries } from '../Controllers/AddTimetable.js';
+import { addTimetableEntries, getAllTimetableEntries, getTimetableEntriesById, } from '../Controllers/AddTimetable.js';
 
 const router = express.Router();
 
 router.post('/add', addTimetableEntries);
 router.get('/display-timetable', getAllTimetableEntries);
+router.get('/display-timetable/:id', getTimetableEntriesById);
+
 
 export default router;
